fecda3dd49c6a4787831fbfe9fdc6a09cb3bb2e4
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2023-07-22 20:42:46 +0530

    Vectors

---
+++ 35_5_Vectors.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+template <class T>
+
+void display(vector<T> &v){
+    cout<<1;
+}
+
+int main(){
+    vector<int> v1(35,20);
+    return 0;
+}
